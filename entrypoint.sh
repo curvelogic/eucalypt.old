31ea33bc47f0c11d4e3fb807feb199f56a3ecd8a
Greg Hawkins <github@gmorpheme.net>
2018-04-24 11:53:34 +0100

    eut.py acceptance test suite (#1)
    * Gemfile for local docs testing.
    
    * Basic eut.py test harness to run the test/* directory shebangs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,24 +3,21 @@
 set -e
 
 if [ ! -f $EXECUTABLE ] ; then
-    echo "$EXECUTABLE does not identify a file; please set to path of Eucalypt command line to test"
-    exit 1
+    echo "WARNING! $EXECUTABLE does not identify a file; please set to path of Eucalypt command line to test"
+else
+    echo "Preparing executable $EXECUTABLE"
+    mkdir -p local/bin
+    cp $EXECUTABLE local/bin/${EXECUTABLE##*/}
+    chmod +x $EXECUTABLE
+    export PATH=local/bin:$PATH
+
+    echo "PATH is now $PATH, testing `eu` command:"
+
+    eu --help
+
+    if [ $? -ne 0 ] ; then
+        echo "WARNING! eu binary is not executable on this architecture"
+    fi
 fi
 
-
-echo "Preparing executable $EXECUTABLE"
-mkdir -p local/bin
-cp $EXECUTABLE local/bin/${EXECUTABLE##*/}
-chmod +x $EXECUTABLE
-export PATH=local/bin:$PATH
-
-echo "PATH is now $PATH, testing `eu` command:"
-
-eu --help
-
-if [ $? -ne 0 ] ; then
-    echo "eu binary is not executable on this architecture"
-    exit 1
-fi
-    
 exec "$@"
