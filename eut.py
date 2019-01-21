84a87bd9925e9a566a0639f2acd6c856497eaeab
gmorpheme <github@gmorpheme.net>
2019-01-21 14:13:14 +0000

    Allow override of output directory.

--- eut.py
+++ eut.py
@@ -17,13 +17,6 @@ import json
 from enum import Enum
 from pathlib import (Path)
 
-test_directory = Path("test")
-output_directory = Path(".build/output")
-
-def prepare_directories():
-    if not test_directory.is_dir():
-        raise RuntimeError("Cannot locate test directory")
-
 class Result(Enum):
     SUCCESS = 1
     IGNORE = 2
@@ -35,16 +28,16 @@ class Test:
 
     RE = re.compile(r"(x?)(\d+)_(.*?).(eu|yaml|json|toml)")
 
-    def __init__(self, filepath, format="yaml"):
+    def __init__(self, filepath, outdir, format="yaml"):
         self.filepath = filepath
         (ignore, id, name, ext) = Test.RE.fullmatch(filepath.name).groups()
         self.ignore = ignore == 'x'
         self.id = id
         self.name = name
         self.format = format
-        self.outfile = output_directory / filepath.relative_to(test_directory).with_suffix(f".out.{format}")
+        self.outfile = outdir / filepath.relative_to(test_directory).with_suffix(f".out.{format}")
         self.outfile.parent.mkdir(parents = True, exist_ok = True)
-        self.errfile = output_directory / filepath.relative_to(test_directory).with_suffix(f".{format}.err")
+        self.errfile = outdir / filepath.relative_to(test_directory).with_suffix(f".{format}.err")
         self.errfile.parent.mkdir(parents = True, exist_ok = True)
         self.eu_args = ["eu", "-B", "-x", format, str(filepath)]
 
@@ -131,16 +124,16 @@ class BenchmarkTest(Test):
 
     """   A test run purely for timing.   """
 
-    def __init__(self, filepath, format="yaml"):
-        super().__init__(filepath, format)
+    def __init__(self, filepath, outdir, format="yaml"):
+        super().__init__(filepath, outdir, format)
         self.id = "B" + self.id
 
 class ErrorTest(Test):
 
     """A test that expects a non-zero returncode"""
 
-    def __init__(self, filepath, format="yaml"):
-        super().__init__(filepath, format)
+    def __init__(self, filepath, outdir, format="yaml"):
+        super().__init__(filepath, outdir, format)
         self.id = "E" + self.id
 
     def check(self):
@@ -152,16 +145,16 @@ class ShellTest:
     """A test that runs a shell script and requires a zero returncode"""
     RE = re.compile(r"(x?)(\d+)_(.*?).sh")
 
-    def __init__(self, filepath):
+    def __init__(self, filepath, outdir):
         self.filepath = filepath
         (ignore, id, name) = ShellTest.RE.fullmatch(filepath.name).groups()
         self.id = "S" + id
         self.ignore = ignore == 'x'
         self.name = name
         self.format = format
-        self.outfile = output_directory / filepath.relative_to(test_directory).with_suffix(f".out.{format}")
+        self.outfile = outdir / filepath.relative_to(test_directory).with_suffix(f".out.{format}")
         self.outfile.parent.mkdir(parents = True, exist_ok = True)
-        self.errfile = output_directory / filepath.relative_to(test_directory).with_suffix(f".{format}.err")
+        self.errfile = outdir / filepath.relative_to(test_directory).with_suffix(f".{format}.err")
         self.errfile.parent.mkdir(parents = True, exist_ok = True)
 
 
@@ -229,38 +222,48 @@ class ShellTest:
         return self
 
 
-def find_simple_tests():
-    return [Test(p,fmt) for (p, fmt) in
-            product(sorted(chain(test_directory.glob("*.eu"),
-                                 test_directory.glob("*.yaml"),
-                                 test_directory.glob("*.json"),
-                                 test_directory.glob("*.toml"))),
+def find_simple_tests(testdir, outdir):
+    return [Test(p, outdir, fmt) for (p, fmt) in
+            product(sorted(chain(testdir.glob("*.eu"),
+                                 testdir.glob("*.yaml"),
+                                 testdir.glob("*.json"),
+                                 testdir.glob("*.toml"))),
                            ["yaml", "json"])]
 
-def find_error_tests():
-    return [ErrorTest(p) for p in sorted(test_directory.glob("errors/*.eu"))]
+def find_error_tests(testdir, outdir):
+    return [ErrorTest(p, outdir) for p in sorted(testdir.glob("errors/*.eu"))]
 
-def find_benchmark_tests():
-    return [BenchmarkTest(p) for p in sorted(test_directory.glob("bench/*.eu"))]
+def find_benchmark_tests(testdir, outdir):
+    return [BenchmarkTest(p, outdir) for p in sorted(testdir.glob("bench/*.eu"))]
 
-def find_shell_tests():
-    return [ShellTest(p) for p in sorted(test_directory.glob("shell/*.sh"))]
+def find_shell_tests(testdir, outdir):
+    return [ShellTest(p, outdir) for p in sorted(testdir.glob("shell/*.sh"))]
+
+test_directory = Path("test")
+default_output_directory = Path(".build/output")
+
+def prepare_directories():
+    if not test_directory.is_dir():
+        raise RuntimeError("Cannot locate test directory")
 
 parser = argparse.ArgumentParser("Eucalypt test harness")
 parser.add_argument("-b", "--bench", action='store_true', default=False)
 parser.add_argument("-n", "--repeats", action='store', type=int, default=100)
+parser.add_argument("-o", "--output-directory", action='store', default=default_output_directory)
 
 def main():
     opts = parser.parse_args()
     prepare_directories()
-    tests = find_simple_tests() + find_error_tests() + find_shell_tests()
+    tests = find_simple_tests(test_directory, opts.output_directory) + \
+            find_error_tests(test_directory, opts.output_directory) + \
+            find_shell_tests(test_directory, opts.output_directory)
 
     if opts.bench:
         print(f"Timing standard tests with {opts.repeats} repeats.")
         results = [t.bench(opts.repeats) for t in tests]
 
         print(f"Timing benchmark tests without repeat.")
-        results = [t.bench(1) for t in find_benchmark_tests()]
+        results = [t.bench(1) for t in find_benchmark_tests(test_directory, opts.output_directory)]
     else:
         results = [t.run() for t in tests]
         if any(r.failed() for r in results):
